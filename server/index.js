4a239a8922f1993cd74217874fa247cdb3fceda8
Ermel Laxamana <Ermel.Laxamana@filamsoftware.com>
2022-08-12 18:57:32 +0800

    added .env

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log('Connected')
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Server Running')
